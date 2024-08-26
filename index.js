698b4c5a0eb3767a3bc1b0702ac87791e1072ff5
ali <princeali4503050@gmail.com>
2024-08-26 15:20:56 +0500

    JavaScript practice

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('check it')
+
+function countWovel(vowel){
+    // debugger
+    let count = 0;
+    for(vou in vowel){
+        if(['a','e','i','o','u'].includes(vowel[vou])){
+            count = count + 1
+            // console.log(vowel[vou])
+         }
+        // console.log(count)
+    }
+
+    return count
+}
+
+let result = countWovel('awais')
+// console.log(result)
+
+
+// reverse indexing functionality
+function reverseIndexing(keyWord){
+    for(let i = keyWord.length; i >= 0; i--){
+        console.log(i)
+    }
+}
+
+reverseIndexing('khan')
+
+// Map Method functionality
+
+function customMap(array, callback){
+    let newarray = []
+    for(let item of array){
+        newarray.push(callback(item))
+    }
+    return newarray
+}
